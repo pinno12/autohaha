dc50c71d79d1a6afcf70039ea780b0971bd660bb
Won <pinno12@kaist.ac.kr>
2019-01-28 22:00:56 +0900

    Create sms_app.js

---
+++ sms_app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+
+//connect check to DB
+let models = require('./models');
+models.sequelize.sync()
+  .then(function () {
+    console.log('✓ DB connection success.');
+    console.log('  Press CTRL-C to stop\n');
+  }).catch(function (err) {
+    console.error(err);
+    log.error(err);
+    console.log('✗ DB connection error. Please make sure DB is running.');
+    process.exit();
+  });
+
+module.exports = app;
